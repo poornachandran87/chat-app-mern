463f5cfa9720a621b4f64981e7571fece8d38127
poornachandran872 <chandru08july2000@gmail.com>
2024-10-22 11:34:03 +0530

    socket part added

---
+++ backend/socket/socket.js
@@ -0,0 +1,38 @@
+import { Server  } from "socket.io";
+import http from 'http';
+import express from 'express';
+
+
+const app = express()
+
+const server = http.createServer(app)
+
+const io = new Server(server,{
+    cors:{
+        origin: ["http://localhost:3000"],
+        methods: ["GET","POST"]
+    }
+});
+
+export const getReceiverSocketId = (receiverId) => {
+    return userSocketMap[receiverId]
+}
+const userSocketMap ={}
+
+io.on("connection",(socket) =>{
+    console.log("connection",socket.id)
+
+    const userID= socket.handshake.query.userID;
+    if(userID != "undefined") userSocketMap[userID] =socket.id
+
+    io.emit("getOnlineUser",Object.keys(userSocketMap))
+
+    socket.on("disconnect",() => {
+        console.log("user disconnected",socket.id)
+        delete userSocketMap[userID]
+        io.emit("getOnlineUser",Object.keys(userSocketMap))
+
+    })
+})
+
+export { app,io,server }
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,6 +3,7 @@ import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from "react-icons/ti"
 import useConversation from '../../zustand/useConversation.js'
+import { useAuthContext } from '../../context/AuthContext.jsx'
 
 const MessageContainer = () => {
     
@@ -31,10 +32,11 @@ export default MessageContainer;
 
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
     return(
         <div className='flex items-center justify-center w-full h-full'>
             <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                <p>Welcome 👋 Jhon Doe 💌</p>
+                <p>Welcome 👋 {authUser.username} 💌</p>
                 <p>Select a chat to start messaging</p>
                 <TiMessages className='text-3xl md:text-6xl text-center' />
             </div>
